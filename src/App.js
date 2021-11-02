202bfd58241dc16c56d6fe213fd02b5a1d1e9632
minasoha <minasoha@yahoo.com>
2021-11-02 19:14:40 -0400

    finished everything

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import AddMovieForm from './components/AddMovieForm';
-import FavoriteMovieList from './components/FavoriteMovieList';
+import AddMovieForm from "./components/AddMovieForm";
+import FavoriteMovieList from "./components/FavoriteMovieList";
 
-const App = props => {
+const App = (props) => {
   const displayFavorites = true;
 
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/>Redux Module Project</span>
+        <span className="navbar-brand">
+          <img width="40px" alt="" src="./Lambda-Logo-Red.png" />
+          Redux Module Project
+        </span>
       </nav>
 
       <div className="container">
-        <MovieHeader/>
+        <MovieHeader />
         <div className="row ">
-          {displayFavorites && <FavoriteMovieList/>}
-        
+          {displayFavorites && <FavoriteMovieList />}
+
           <Switch>
             <Route exact path="/movies/add">
               <AddMovieForm />
@@ -35,11 +38,11 @@ const App = props => {
             </Route>
 
             <Route path="/movies">
-              <MovieList/>
+              <MovieList />
             </Route>
 
             <Route path="/">
-              <Redirect to="/movies"/>
+              <Redirect to="/movies" />
             </Route>
           </Switch>
         </div>
@@ -48,4 +51,15 @@ const App = props => {
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    id: state.movieState.movies.id,
+    title: state.movieState.movies.title,
+    director: state.movieState.movies.director,
+    genre: state.movieState.movies.genre,
+    metascore: state.movieState.movies.metascore,
+    description: state.movieState.movies.description,
+  };
+};
+
+export default connect(mapStateToProps)(App);
